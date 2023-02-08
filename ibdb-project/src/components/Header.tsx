878c77697904da60d2bd7c78342ea10210f7590a
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-02-08 22:18:14 +0100

    #10 Start header-design
    Header has basic setup with search-bar and buttons for other pages

--- ibdb-project/src/components/Header.tsx
+++ ibdb-project/src/components/Header.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div >
-            <input type="text"/>
+        <div className="header">
+            <div></div>
+            <div>
+                <input className="searchBar" type="text" placeholder="Search"/>
+            </div>
+            <div className="rightSection">
+                <button>
+                    <Link to="addBook" className="buttonStyle">Add Book</Link>
+                </button>
+                <button>
+                    <Link to="myBookLists" className="buttonStyle">My Lists</Link>
+                </button>
+                <button>
+                    <Link to="ratedBooks" className="buttonStyle">Rated Books</Link>
+                </button>
+                <button>
+                    <Link to="loginPage" className="buttonStyle">Login</Link>
+                </button>
+            </div>
         </div>
     )
 }
--- ibdb-project/src/pages/HomePage.tsx
+++ ibdb-project/src/pages/HomePage.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Book from '../components/Book';
 
 const HomePage = () => {
     return (
         <div>
             <Header/>
-            <Link to="loginPage">Login</Link>
-            <Link to="addBook">Add Book</Link>
-            <Link to="myBookLists">My Lists</Link>
-            <Link to="ratedBooks">Rated Books</Link>
+            
+            <Book/>
 
         </div>
     )
