3120a77e78fac1f9c6c021ef456b50560fc2d593
Filip Skaug <filis@stud.ntnu.no>
2023-02-12 12:12:52 +0100

    fix header links + style button

--- ibdb-project/src/components/Header.tsx
+++ ibdb-project/src/components/Header.tsx
@@ -4,22 +4,22 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="navbar navbar-expand-lg shadow-md py-5 px-10 bg-bigBoy relative flex items-center w-full justify-between">
+        <div className="navbar navbar-expand-lg shadow-md py-5 px-10 relative flex items-center w-full justify-between">
             <button>
-                <Link to="/" className="px-5 py-2 rounded-lg bg-kulTheme dark:hover:bg-teitThene font-serif text-3xl shadow" >IBDb</Link>
+                <Link to="/" className="px-5 py-2 rounded-lg bg-kulTheme dark:hover:bg-teitThene font-serif text-3xl shadow-0 hover:shadow-lg" >IBDb</Link>
             </button>
             <button>
-                <Link to=" " className="px-5 py-2 rounded-lg bg-hvit shadow" >Meny</Link>
+                <Link to=" " className="px-5 py-2 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Meny</Link>
             </button>
             <input
                 type="text"
-                className="block w-2/3 px-4 py-2 text-purple-700 bg-white rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 shadow"
+                className="block w-2/3 px-4 py-2 text-purple-700 bg-white rounded-full focus:ring-bigBoy focus:outline-none focus:ring focus:ring-opacity-40 shadow-inner"
                 placeholder="Search..."/>
             <button>
-                <Link to=" " className="px-5 py-2 rounded-lg bg-hvit shadow" >Filter</Link>
+                <Link to=" " className="px-5 py-2 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Filter</Link>
             </button>
             <button>
-                <Link to="loginPage" className="px-5 py-2 rounded-lg bg-hvit shadow" >profil</Link>
+                <Link to="loginPage" className="px-5 py-2 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >profil</Link>
             </button>
 
         </div>
