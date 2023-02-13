b0a54efc5cb28d7543b647b8c87ce119ed2e37f9
Synne Kvamme <synnekva@gmail.com>
2023-02-13 14:05:27 +0100

    Working on several filters
    Made a dropdown button to make it possible to select several filters.
    Also made buttons appear when you select.
    
    Issue #13

--- ibdb-project/src/components/Filter.tsx
+++ ibdb-project/src/components/Filter.tsx
@@ -30,26 +30,87 @@ const Filter = () => {
             firebaseController
                 .getBooks()
                 .then(books => setBooks(books.filter(book => book.genre === "Fiction")));
+
         }
         return
     }, [filter]);
 
     console.log(books);
 
+    const [clicked, setClicked] = React.useState(false);
+    const handleClicked = () => {
+        setClicked(!clicked);
+      };
+
+    const [fictionChosen, setFictionChosen] = React.useState(false);
+    const [romanChosen, setRomanChosen] = React.useState(false);
+    const [crimeChosen, setCrimeChosen] = React.useState(false);
+
+    const handleFictionChosen = () => {
+        setFictionChosen(!fictionChosen); 
+    };
+
+    const handleRomanChosen = () => {
+        setRomanChosen(!romanChosen); 
+    };
+
+    const handleCrimeChosen = () => {
+        setCrimeChosen(!crimeChosen); 
+    };
+
     return (
         <div>
             <div className="navbar navbar-expand-lg shadow-md pb-5 px-10 bg-bigBoy relative flex items-center w-full justify-center space-x-10">
                 <p>Filtere: </p>
-                <button>
-                    <Link to="/filteredBooks/highestRated" className="px-5 py-2 rounded-lg bg-hvit shadow" >Høyest rating</Link>
-                </button>
+                <div className="space-y-2 relative bg-current">
+                    <button onClick={handleClicked} className="px-5 py-2 rounded-lg bg-hvit shadow">
+                        Sjanger
+                    </button>
+                    {clicked ? 
+                     <div className='navbar navbar-expand-lg shadow-md absolute space-y-3 bg-current'>
+                        <button onClick={handleFictionChosen}>
+                            <Link to="/filteredBooks/fiction" className="px-5 py-2 rounded-lg bg-hvit shadow" >Fiction</Link>
+                        </button>
+                        <button onClick={handleCrimeChosen}>
+                            <Link to="/filteredBooks/fiction" className="px-5 py-2 rounded-lg bg-hvit shadow" >Crime</Link>
+                        </button>
+                        <button onClick={handleRomanChosen}>
+                            <Link to="/filteredBooks/fiction" className="px-5 py-2 rounded-lg bg-hvit shadow" >Roman</Link>
+                        </button>
+                     </div>
+                    : null}
+                </div>
+            
                 <button>
                     <Link to="/filteredBooks/newest" className="px-5 py-2 rounded-lg bg-hvit shadow" >Nyeste</Link>
                 </button>
                 <button>
                     <Link to="/filteredBooks/fiction" className="px-5 py-2 rounded-lg bg-hvit shadow" >Fiction</Link>
                 </button>
+
+                <div>
+                    {fictionChosen ?
+                        <button className="px-2 py-1 rounded-lg bg-slate-400 shadow">
+                            <p className='text-sm'>fiction</p>
+                        </button> : null
+                    }
+                    {crimeChosen ?
+                        <button className="px-2 py-1 rounded-lg bg-slate-400 shadow">
+                            <p className='text-sm'>crime</p>
+                        </button> : null
+                    }
+                    {romanChosen ?
+                        <button className="px-2 py-1 rounded-lg bg-slate-400 shadow">
+                            <p className='text-sm'>roman</p>
+                        </button> : null
+                    }
+                    
+                </div>
             </div>
+            
+            
+
+
             <div className="grid grid-cols-3">
                 {books.map((book) => (
                     <div key={book.id} className="grid place-items-center border-4 ml-100">
