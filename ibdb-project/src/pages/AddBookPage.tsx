f8b2a70f2dc0d185c33968e5c8ab1fb8cea81baa
Mathea Bardalgit commit <matheaab@stud.ntnu.no>
2023-02-13 11:10:33 +0100

    fix merge problems

--- ibdb-project/src/pages/AddBookPage.tsx
+++ ibdb-project/src/pages/AddBookPage.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from '../components/Header';
 
 
 const AddBookPage = () => {
     return (
         <div>
-            <Header/>
             <h1>
                 This is AddBook
             </h1>
