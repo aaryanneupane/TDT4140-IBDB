290f315754fa5d8329a9ad9143c1263ce6c256c0
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-02-17 11:06:29 +0100

    #16 fix-filter-import
    Fix so there can be no errors in main
    
    Co-authored-by: filis <filis@stud.ntnu.no>

--- ibdb-project/src/components/Header.tsx
+++ ibdb-project/src/components/Header.tsx
@@ -1,49 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import DownDrop from './DownDrop';
 import '../styles/Header.css';
+import Filter from './Filter';
 
 const Header = () => {
-    let dDelements = [{id: 1, name: "1 item", link: "https://instabart.no/"}, {id: 2, name: "2 item", link: "https://instabart.no/"}]
-    return (
-        <div className="sticky top-0 z-30 navbar navbar-expand-lg shadow-md py-5 px-10 relative flex items-center w-full justify-between bg-bigBoy">
-            <button>
-                <Link to="/" className="px-5 py-2 rounded-lg bg-kulTheme dark:hover:bg-teitThene font-serif text-4xl shadow-0 hover:shadow-lg " >IBDb</Link>
-            </button>
-            {/* <DownDrop elements={dDelements}/> */}
-            <DownDrop/>
-            <input
-                type="text"
-                className="block w-2/3 px-4 py-4 text-purple-700 bg-white rounded-full focus:ring-bigBoy focus:outline-none focus:ring focus:ring-opacity-40 shadow-inner"
-                placeholder="Search..."/>
-            <button>
-                <Link to=" " className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Filter</Link>
-            </button>
-            <button>
-                <Link to=" " className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Profile</Link>
-            </button>
 
-    const navigate = useNavigate();
+    // const [filterClicked, setFilterClicked] = useState(false);
+
+
+    let dDelements = [{ id: 1, name: "1 item", link: "https://instabart.no/" }, { id: 2, name: "2 item", link: "https://instabart.no/" }]
 
     return (
-        <div>
-            <div className="navbar navbar-expand-lg shadow-md py-5 px-10 bg-bigBoy relative flex items-center w-full justify-between">
+        <div className="sticky top-0 z-30 navbar navbar-expand-lg shadow-md py-5 px-10 relative bg-bigBoy">
+            <div className ="flex items-center w-full justify-between">
                 <button>
-                    <Link to="/" className="px-5 py-2 rounded-lg bg-kulTheme dark:hover:bg-teitThene font-serif text-3xl shadow" >IBDb</Link>
+                    <Link to="/" className="px-5 py-2 rounded-lg bg-kulTheme dark:hover:bg-teitThene font-serif text-4xl shadow-0 hover:shadow-lg " >IBDb</Link>
                 </button>
-                <button>
-                    <Link to=" " className="px-5 py-2 rounded-lg bg-hvit shadow" >Meny</Link>
-                </button>
+                {/* <DownDrop elements={dDelements}/> */}
+                <DownDrop />
                 <input
                     type="text"
-                    className="block w-2/3 px-4 py-2 text-purple-700 bg-white rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 shadow"
+                    className="block w-2/3 px-4 py-4 text-purple-700 bg-white rounded-full focus:ring-bigBoy focus:outline-none focus:ring focus:ring-opacity-40 shadow-inner"
                     placeholder="Search..." />
+                {/* <button onClick={() => setFilterClicked(!filterClicked)}> */}
                 <button>
-                    <Link to="/loginPage" className="px-5 py-2 rounded-lg bg-hvit shadow" >profil</Link>
+                    <Link to="" className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Filter</Link>
+                </button>
+                
+                <button>
+                    <Link to=" " className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg" >Profile</Link>
                 </button>
             </div>
-        </div>
-    )
+            {/* <div>
+                { filterClicked ?
+                <Filter/>
+                : null
+                }
+            </div> */}
+        </div>)
 }
 
 export default Header;
+
