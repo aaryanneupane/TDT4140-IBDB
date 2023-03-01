a7d4db21b19c2d302e2b9205c0bd4e0479636b98
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-03-01 23:00:53 +0100

    #26 add errorMessage
    Added some errorMessages to show up if there was an error

--- ibdb-project/src/components/LoginPopup.tsx
+++ ibdb-project/src/components/LoginPopup.tsx
@@ -1,24 +1,43 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 import '../styles/LoginPopup.css';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { AuthError, AuthErrorCodes, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebaseControl';
 
 const LoginPopup = ({ visible, setVisible }: { visible: boolean; setVisible: Dispatch<SetStateAction<boolean>> }) => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const reset = () => {
+        setVisible(false);
+        setEmail('');
+        setPassword('');
+        setErrorMessage('');
+    }
 
     const logIn = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredentials) => {
                 console.log(userCredentials);
-                setVisible(false);
-                setEmail('');
-                setPassword('');
+                reset();
             }).catch((error) => {
                 console.log(error);
+                showError(error);
             });
+    }
+
+    const showError = (error: AuthError) => {
+        if (error.message == 'Firebase: Error (auth/invalid-email).') {
+            setErrorMessage('Invalid Email')
+        } else if (error.message == 'Firebase: Error (auth/user-not-found).') {
+            setErrorMessage('No user with this email');
+        } else if (error.message == 'Firebase: Error (auth/wrong-password).') {
+            setErrorMessage('Wrong password, try again');
+        } else if (error.message == 'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).') {
+            setErrorMessage('Access to this account has been temporarily disabled due to many failed login attempts');
+        }
     }
 
     const signUp = (e: { preventDefault: () => void; }) => {
@@ -26,6 +45,7 @@ const LoginPopup = ({ visible, setVisible }: { visible: boolean; setVisible: Dis
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredentials) => {
                 console.log(userCredentials);
+                reset();
             }).catch((error) => {
                 console.log(error);
             });
@@ -35,11 +55,14 @@ const LoginPopup = ({ visible, setVisible }: { visible: boolean; setVisible: Dis
             {visible ?
                 <div className="login">
                     <div className="login-inner rounded-lg">
-                        <input className="px-6 py-4 bg-hvit shadow-0 hover:shadow-lg" id="email" type="text" placeholder="Email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-                        <input className="px-6 py-4 bg-hvit shadow-0 hover:shadow-lg" id="password" type="password" placeholder="Password" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
-                        <button className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg login-close" onClick={() => { setVisible(false) }}>
+                        <input className="px-6 py-4 bg-hvit shadow-0 hover:shadow-lg" id="email" type="text" placeholder="Email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                        <input className="px-6 py-4 bg-hvit shadow-0 hover:shadow-lg" id="password" type="password" placeholder="Password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                        <button className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg login-close" onClick={() => { reset() }}>
                             Close
                         </button>
+                        {errorMessage != '' ?
+                            <p className='error'>{errorMessage}</p>
+                            : null}
                         <button className="px-6 py-3 rounded-lg bg-hvit shadow-0 hover:shadow-lg" onClick={logIn}>
                             Log in
                         </button>
