985a92e140fdda0aac7592f9881475a117f3e38a
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-03-14 16:01:10 +0100

    #38 test card, bookpage, downdrop and popup
    Made 5 tests for card, bookpage, downdrop and popup
    
    Co-authored-by: Mathea Bardal <matheaab@stud.ntnu.no>

---
+++ ibdb-project/src/tests/Card.test.tsx
@@ -0,0 +1,13 @@
+import { act, fireEvent, render } from '@testing-library/react';
+import Card from '../components/Card';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+test('navigate to correct bookpage when clicked', () => {
+    const {getByRole} = render(<Router><Card title={'Spare'} bookIMG={'https://upload.wikimedia.org/wikipedia/en/6/69/Spare_cover.jpg'} id={'4'}/></Router>);
+    const cardImg = getByRole("img");
+    act(() => {
+        fireEvent.click(cardImg);
+    })
+    const pathName = window.location.pathname;
+    expect(pathName).toBe("/bookPage/4");
+})
